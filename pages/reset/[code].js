38d2718544c70d7ed0c4c41b6499fd00f49c85b4
Otavio Jacobi <o.jacobi@sap.com>
2021-05-09 19:19:27 -0300

    more attempts

--- pages/reset/[code].js
+++ pages/reset/[code].js
@@ -23,21 +23,25 @@ import useStyles from "../../src/loginRegisterStyle";
 
 export default function Reset() {
   const classes = useStyles();
+  const router = useRouter();
 
   const [changePwdFailed, setChangePwdFailed] = useState(false);
   const [isChangingPwd, setIsChangingPwd] = useState(false);
 
   const sendResetEmail = async (event) => {
 
-    const router = useRouter();
 
     event.preventDefault();
 
     setIsChangingPwd(true);
 
+    console.log('quero');
+
     const password = event.target.password.value;
     const passwordConfirmation = event.target.passwordConfirmation.value;
     const code = router.query.code;
+
+    console.log(code);
 
     const res = await fetch(
       `${process.env.MY_TRAINER_BACKEND}/auth/reset-password`,
